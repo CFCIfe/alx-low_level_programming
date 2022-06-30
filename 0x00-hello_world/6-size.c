da3020da994a3b080affe425099600c0713296f5
root <aboludepeter@gmail.com>
2022-06-30 06:31:52 -0700

    added 0x00-hello_world/6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - This is a description
+ * Return: a number 0 if successful
+ */
+int main(void)
+{
+	printf("Size of a char: %ld bytes(s)\n", sizeof(char));
+	printf("Size of an int: %ld bytes(s)\n", sizeof(int));
+	printf("Size of a long int: %ld bytes(s)'n", sizeof(long int));
+	printf("Size of a long long int: %ld bytes(s)", sizeof(long long int));
+	printf("Size of a float: %ld bytes(s)", sizeof(float)
+	return (0);
+}
