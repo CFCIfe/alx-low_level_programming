8b2cb3f6a3a24e461e52f04490fa82d63121c78f
root <aboludepeter@gmail.com>
2022-06-30 05:36:56 -0700

    added 0x00-hello_world/4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
-int main()
+/**
+ * main - This is a description
+ *
+ * Return: a number 0 if successful
+ */
+int main(void)
 {
 	puts("Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
