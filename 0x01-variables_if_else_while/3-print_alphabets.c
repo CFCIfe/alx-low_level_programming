d2a34057c1a1ed73dcf943da9273e4b8e4cf8b72
root <aboludepeter@gmail.com>
2022-07-01 07:04:08 -0700

    added 3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - function
+ *
+ * Return: 0 if successful
+ *
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'A'; ch <= 'Z'; ch++)
+	{
+		putchar(ch);
+	}
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
