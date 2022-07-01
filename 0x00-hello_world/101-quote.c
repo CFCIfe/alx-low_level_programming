b20b3bca718f494289556d72333e4605cdba8d4c
root <aboludepeter@gmail.com>
2022-07-01 01:27:58 -0700

    Add 101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include<unistd.h>
+/**
+ * main - This is a description
+ * Return: a number 1 if successful
+ */
+
+int main(void)
+{
+	write(1,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 30);
+}
