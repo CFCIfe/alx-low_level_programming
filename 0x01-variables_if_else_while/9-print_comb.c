acd850e3043dee758ba2a601b4ba92246994f557
root <aboludepeter@gmail.com>
2022-07-02 07:58:46 -0700

    Added 9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - function
+ *
+ * Return: 0 if successful
+ *
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 0 ; n < 10 ; n++)
+	{
+		putchar(n + '0');
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
