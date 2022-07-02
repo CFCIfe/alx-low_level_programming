f681a7d704f7c14b29629d754e7117fe899ed991
root <aboludepeter@gmail.com>
2022-07-02 00:45:52 -0700

    added 6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - function
+ *
+ * Return: 0 if successful
+ *
+ */
+
+int main(void)
+{
+	int n;
+
+	for (int n = 0 ; n < 10 ; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
