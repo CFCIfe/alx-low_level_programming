ad01b103773569521d8d4eecc046e7a1c739315d
Peter Abolude <aboludepeter@gmail.com>
2023-06-19 14:54:52 +0000

    betty styled

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -6,17 +6,33 @@
  * @name: Name of the dog
  * @age: Age of the dog
  * @owner: Owner of the dog
+ *
+ * Description: Structure representing a dog with its name, age, and owner.
  */
 struct dog
 {
-char *name;
-float age;
-char *owner;
+char *name;  /* Name of the dog */
+float age;   /* Age of the dog */
+char *owner; /* Owner of the dog */
 };
+
+/**
+ * init_dog - Initializes a variable of type struct dog
+ * @d: Pointer to the struct dog variable to initialize
+ * @name: Pointer to a string representing the dog's name
+ * @age: Age of the dog
+ * @owner: Pointer to a string representing the owner's name
+ *
+ * Description: This function inits a struct dog variable with the provided
+ *name, age, and owner values. It takes a pointer to the struct dog
+ *variable as its parameter and assigns the values to the members of
+ *the structure.
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner);
 
 /**
  * dog_t - Typedef for struct dog
  */
-void init_dog(struct dog *d, char *name, float age, char *owner);
 typedef struct dog dog_t;
+
 #endif /* DOG_H */
