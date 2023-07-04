cdc68c7979968b37ccf8b47ac795846bc2a6d0ba
Peter Abolude <aboludepeter@gmail.com>
2023-07-04 11:23:58 +0000

    3-add_node_end betty

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -16,16 +16,13 @@ list_t *last_node;
 char *dup_str;
 size_t len = 0;
 
-/* Count the length of the string */
 while (str[len] != '\0')
 len++;
 
-/* Duplicate the string */
 dup_str = strdup(str);
 if (dup_str == NULL)
 return (NULL);
 
-/* Create a new node */
 new_node = malloc(sizeof(list_t));
 if (new_node == NULL)
 {
@@ -33,22 +30,20 @@ free(dup_str);
 return (NULL);
 }
 
-/* Set the data of the new node */
 new_node->str = dup_str;
 new_node->len = len;
 new_node->next = NULL;
 
-/* If the list is empty, set the new node as the head */
 if (*head == NULL)
 {
 *head = new_node;
 return (new_node);
 }
-/* Find the last node in the list */
+
 last_node = *head;
 while (last_node->next != NULL)
 last_node = last_node->next;
-/* Append the new node at the end of the list */
+
 last_node->next = new_node;
 return (new_node);
 }
