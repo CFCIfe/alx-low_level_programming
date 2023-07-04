b639c5c475dd9973c04db425e957d52ad1b4bf55
Peter Abolude <aboludepeter@gmail.com>
2023-07-04 12:11:50 +0000

    betty check

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -3,9 +3,10 @@
 void pre_main_message(void) __attribute__((constructor));
 
 /**
- * pre_main_message - Function that prints a message before the main function is executed.
+ * pre_main_message - fn that prints a msg before the main fnc is executed.
  */
 void pre_main_message(void)
 {
-    printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+printf("You're beat! and yet, you must allow,\n"
+"I bore my house upon my back!\n");
 }
