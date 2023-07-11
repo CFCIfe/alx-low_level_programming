591e9c2e6fb1a330499a7c4388f997f088c2a3e2
Peter Abolude <aboludepeter@gmail.com>
2023-07-11 07:45:43 +0000

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -3,14 +3,15 @@
 #define BUF_SIZE 1024
 
 /**
- * error_exit - Print error message and exit with specified code
+ * error_exit - Print error message to stderr and exit with specified code
  * @code: The exit code
  * @message: The error message to print
+ * @filename: The name of the file related to the error
  */
-void error_exit(int code, const char *message)
+void error_exit(int code, const char *message, const char *filename)
 {
-    dprintf(STDERR_FILENO, "%s\n", message);
-    exit(code);
+fprintf(stderr, "%s %s\n", message, filename);
+exit(code);
 }
 
 /**
@@ -21,35 +22,35 @@ void error_exit(int code, const char *message)
  */
 int main(int argc, char **argv)
 {
-    int file_from, file_to, bytes_read, bytes_written;
-    char buf[BUF_SIZE];
+int file_from, file_to, bytes_read, bytes_written;
+char buf[BUF_SIZE];
 
-    if (argc != 3)
-        error_exit(97, "Usage: cp file_from file_to");
+if (argc != 3)
+error_exit(97, "Usage: cp file_from file_to", "");
 
-    file_from = open(argv[1], O_RDONLY);
-    if (file_from == -1)
-        error_exit(98, "Error: Can't read from file");
+file_from = open(argv[1], O_RDONLY);
+if (file_from == -1)
+error_exit(98, "Error: Can't read from file", argv[1]);
 
-    file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-    if (file_to == -1)
-        error_exit(99, "Error: Can't write to file");
+file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+if (file_to == -1)
+error_exit(99, "Error: Can't write to file", argv[2]);
 
-    while ((bytes_read = read(file_from, buf, BUF_SIZE)) > 0)
-    {
-        bytes_written = write(file_to, buf, bytes_read);
-        if (bytes_written != bytes_read)
-            error_exit(99, "Error: Can't write to file");
-    }
+while ((bytes_read = read(file_from, buf, BUF_SIZE)) > 0)
+{
+bytes_written = write(file_to, buf, bytes_read);
+if (bytes_written == -1 || bytes_written != bytes_read)
+error_exit(99, "Error: Can't write to file", argv[2]);
+}
 
-    if (bytes_read == -1)
-        error_exit(98, "Error: Can't read from file");
+if (bytes_read == -1)
+error_exit(98, "Error: Can't read from file", argv[1]);
 
-    if (close(file_from) == -1)
-        error_exit(100, "Error: Can't close file descriptor");
+if (close(file_from) == -1)
+error_exit(100, "Error: Can't close file descriptor", "");
 
-    if (close(file_to) == -1)
-        error_exit(100, "Error: Can't close file descriptor");
+if (close(file_to) == -1)
+error_exit(100, "Error: Can't close file descriptor", "");
 
-    return (0);
+return (0);
 }
